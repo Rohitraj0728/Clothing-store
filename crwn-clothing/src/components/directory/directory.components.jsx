481a7dc40f1169673e812493d5da473a2dc173af
Rohit Raj <rajrohit19032001@gmail.com>
2021-11-15 17:06:52 +0530

    Added our shop data to its own redux folder.

--- crwn-clothing/src/components/directory/directory.components.jsx
+++ crwn-clothing/src/components/directory/directory.components.jsx
@@ -1,53 +1,20 @@
 import React from 'react';
-
+import { connect } from 'react-redux';
 import MenuItem from '../menu-items/menu-items.components'
+import { selectDirectorySection } from '../redux/directory/directory.selector';
+import { createStructuredSelector } from 'reselect';
 import './directory.styles.scss';
-class Directory extends React.Component {
-constructor(){
-    super();
-
-    this.state = {
-        sections : [
-         {
-             title: 'HATS',
-             imageurl: 'https://i.ibb.co/cvpntL1/hats.png',
-             id: 1
-         },
-         {
-             title: 'JACKETS',
-             imageurl: 'https://i.ibb.co/px2tCc3/jackets.png',
-             id: 1
-         },
-         {
-             title: 'SNEAKERS',
-             imageurl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-             id: 1
-         },
-         {
-             title: 'WOMENS',
-             imageurl: 'https://i.ibb.co/GCCdy8t/womens.png',
-             size: 'large',
-             id: 1
-         },
-         {
-             title: 'MENS',
-             imageurl: 'https://i.ibb.co/R70vBrQ/men.png',
-             size: 'large',
-             id: 1
-         }
-        ]
-    }
-}
-
-render(){
-    return (
+const Directory = ({sections}) => (
         <div className = 'directory-menu'>
-        {this.state.sections.map(({title,imageurl,id,size}) => (
+        {sections.map(({title,imageurl,id,size}) => (
             <MenuItem key = {id} title = {title} imageurl = {imageurl} size = {size} />
         ))}
         </div>
-    )
-}
-}
+    );
 
-export default Directory;
+    const mapStateToProps = createStructuredSelector({
+        sections: selectDirectorySection,
+
+    })
+
+export default connect(mapStateToProps)(Directory);
--- crwn-clothing/src/index.js
+++ crwn-clothing/src/index.js
@@ -5,12 +5,15 @@ import App from './App';
 import {BrowserRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import reportWebVitals from './reportWebVitals';
+import { PersistGate } from 'redux-persist/integration/react';
 
-import store from '../src/components/redux/store';
+import {store , persistor} from '../src/components/redux/store';
 ReactDOM.render(
   <Provider store={store}>
   <BrowserRouter>
+  <PersistGate persistor = {persistor}>
     <App />
+   </PersistGate>
   </BrowserRouter>
   </Provider>,
   document.getElementById('root')
